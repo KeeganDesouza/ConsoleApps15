d4d6f9e912e2bdcb794d672f6f09b2074f255c82
Keegan De souza <104536877+SPIKE445@users.noreply.github.com>
2023-03-01 17:16:00 +0000

    Stage 3 Created 3 new constants along with variables, if statments for new distance choices(Kilometer, Centimeter and Milimeter).

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -19,10 +19,37 @@ namespace ConsoleAppProject.App01
 
         public const double FEET_IN_METERS = 3.28084;
 
+        public const int METERS_IN_KILOMETERS = 1000;
+
+        public const double FEET_IN_KILOMETERS = 3280.839895;
+
+        public const double MILES_IN_KILOMETERS = 1.60935;
+
+        public const int CENTIMETERS_IN_METERS = 100;
+
+        public const int CENTIMETERS_IN_KILOMETERS = 100000;
+
+        public const int CENTIMETERS_IN_MILES = 160935;
+
+        public const double CENTIMETERS_IN_FEET = 30.48;
+
+        public const int METERS_IN_MILIMETERS = 1000;
+
+        public const int MILIMETERS_IN_KILOMETERS = 1000000;
+
+        public const int CENTIMETERS_IN_MILIMETERS = 10;
+
+        public const int MILES_IN_MILIMETERS = 1609350;
+
+        public const double MILIMETERS_IN_FEET = 304.8;
+
         //Distance variables
         private const string FEET = "Feet";
         private const string METERS = "Meters";
         private const string MILES = "Miles";
+        private const string KILOMETERS = "Kilometers";
+        private const string CENTIMETERS = "Centimeters";
+        private const string MILIMETERS = "Milimeters";
 
         //Private Attributes(Global Variables) 
         private double fromDistance;
@@ -84,6 +111,113 @@ namespace ConsoleAppProject.App01
                 toDistance = fromDistance / FEET_IN_METERS;
             }
 
+            if (fromUnit == KILOMETERS && toUnit == METERS)
+            {
+                toDistance = fromDistance * METERS_IN_KILOMETERS;
+            }
+            else if (fromUnit == METERS && toUnit == KILOMETERS)
+            {
+                toDistance = fromDistance / METERS_IN_KILOMETERS;
+            }
+
+            if (fromUnit == KILOMETERS && toUnit == FEET)
+            {
+                toDistance = fromDistance * FEET_IN_KILOMETERS;
+            }
+            else if (fromUnit == FEET && toUnit == KILOMETERS)
+            {
+                toDistance = fromDistance / FEET_IN_KILOMETERS;
+            }
+
+            if (fromUnit == KILOMETERS && toUnit == MILES)
+            {
+                toDistance = fromDistance / MILES_IN_KILOMETERS;
+            }
+            else if (fromUnit == MILES && toUnit == KILOMETERS)
+            {
+                toDistance = fromDistance * MILES_IN_KILOMETERS;
+            }
+
+            if (fromUnit == METERS && toUnit == CENTIMETERS)
+            {
+                toDistance = fromDistance * CENTIMETERS_IN_METERS;
+            }
+            else if (fromUnit == CENTIMETERS && toUnit == METERS)
+            {
+                toDistance = fromDistance / CENTIMETERS_IN_METERS;
+            }
+
+            if (fromUnit == KILOMETERS && toUnit == CENTIMETERS)
+            {
+                toDistance = fromDistance * CENTIMETERS_IN_KILOMETERS;
+            }
+            else if (fromUnit == CENTIMETERS && toUnit == KILOMETERS)
+            {
+                toDistance = fromDistance / CENTIMETERS_IN_KILOMETERS;
+            }
+
+            if (fromUnit == MILES && toUnit == CENTIMETERS)
+            {
+                toDistance = fromDistance * CENTIMETERS_IN_MILES;
+            }
+            else if (fromUnit == CENTIMETERS && toUnit == MILES)
+            {
+                toDistance = fromDistance / CENTIMETERS_IN_MILES;
+            }
+
+            if (fromUnit == FEET && toUnit == CENTIMETERS)
+            {
+                toDistance = fromDistance * CENTIMETERS_IN_FEET;
+            }
+            else if (fromUnit == CENTIMETERS && toUnit == FEET)
+            {
+                toDistance = fromDistance / CENTIMETERS_IN_FEET;
+            }
+
+            if (fromUnit == MILIMETERS && toUnit == METERS)
+            {
+                toDistance = fromDistance / METERS_IN_MILIMETERS;
+            }
+            else if (fromUnit == METERS && toUnit == MILIMETERS)
+            {
+                toDistance = fromDistance * METERS_IN_MILIMETERS;
+            }
+
+            if (fromUnit == KILOMETERS && toUnit == MILIMETERS)
+            {
+                toDistance = fromDistance * MILIMETERS_IN_KILOMETERS;
+            }
+            else if (fromUnit == MILIMETERS && toUnit == KILOMETERS)
+            {
+                toDistance = fromDistance / MILIMETERS_IN_KILOMETERS;
+            }
+
+            if (fromUnit == MILIMETERS && toUnit == CENTIMETERS)
+            {
+                toDistance = fromDistance / CENTIMETERS_IN_MILIMETERS;
+            }
+            else if (fromUnit == CENTIMETERS && toUnit == MILIMETERS)
+            {
+                toDistance = fromDistance * CENTIMETERS_IN_MILIMETERS;
+            }
+
+            if (fromUnit == MILIMETERS && toUnit == MILES)
+            {
+                toDistance = fromDistance / MILES_IN_MILIMETERS;
+            }
+            else if (fromUnit == MILES && toUnit == MILIMETERS)
+            {
+                toDistance = fromDistance * MILES_IN_MILIMETERS;
+            }
+
+            if (fromUnit == FEET && toUnit == MILIMETERS)
+            {
+                toDistance = fromDistance * MILIMETERS_IN_FEET;
+            }
+            else if (fromUnit == MILIMETERS && toUnit == FEET)
+            {
+                toDistance = fromDistance / MILIMETERS_IN_FEET;
+            }
 
         }
         /// <Summary>
@@ -111,7 +245,18 @@ namespace ConsoleAppProject.App01
             {
                 return MILES;
             }
-
+            else if (choice.Equals("4"))
+            {
+                return KILOMETERS;
+            }
+            else if (choice.Equals("5"))
+            {
+                return CENTIMETERS;
+            }
+            else if (choice.Equals("6"))
+            {
+                return MILIMETERS;
+            }
             return null;
         }
         /// <Summary>
@@ -123,6 +268,9 @@ namespace ConsoleAppProject.App01
             Console.WriteLine($" 1. {FEET}");
             Console.WriteLine($" 2. {METERS}");
             Console.WriteLine($" 3. {MILES}");
+            Console.WriteLine($" 4. {KILOMETERS}");
+            Console.WriteLine($" 5. {CENTIMETERS}");
+            Console.WriteLine($" 6. {MILIMETERS}");
             Console.WriteLine();
 
             Console.Write(prompt);
