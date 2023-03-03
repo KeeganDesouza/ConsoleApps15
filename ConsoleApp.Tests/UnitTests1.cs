0e313e1936c3b07e9e2a4e20d1211c50dfc045b6
Keegan De souza <104536877+SPIKE445@users.noreply.github.com>
2023-03-03 16:57:53 +0000

    App01 UML Diagram.

--- ConsoleApp.Tests/UnitTests1.cs
+++ ConsoleApp.Tests/UnitTests1.cs
@@ -1,4 +1,3 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ConsoleAppProject.App01;
 
 namespace UnitTestProject
@@ -12,13 +11,14 @@ namespace UnitTestProject
             DistanceConverter converter = new DistanceConverter();
 
 
-            converter.FromUnit = DistanceConverter.MILES;
-            converter.ToUnit = DistanceConverter.FEET;
+            converter.FromUnit = DistanceConverter.ToString;
+            converter.ToUnit = DistanceConverter.ToString;
 
             converter.FromDistance = 1.0;
             converter.ConvertDistance();
 
             double expectedDistance = 5280;
+            double actual = converter.ToDistance;
 
             Assert.AreEqual(expectedDistance, converter.ToDistance);
               
